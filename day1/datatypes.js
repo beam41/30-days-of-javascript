6216f009a3e128e04c38b55456da0b66e62c72e9
Meehoi <beam_baimon@hotmail.com>
2020-07-02 10:37:51 +0700

    day 1

---
+++ day1/datatypes.js
@@ -0,0 +1,4 @@
+console.log(typeof string);
+console.log(typeof boolean);
+console.log(typeof undef);
+console.log(typeof n);
---
+++ day1/variable.js
@@ -0,0 +1,4 @@
+var string = "string";
+var boolean = true;
+var undef = undefined;
+var n = null;
